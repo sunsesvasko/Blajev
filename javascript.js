dd7a900485024e6b24edbf82f40baa0e05c07280
Vasko Blazhev <vbblajev@gmail.com>
2023-02-26 18:06:10 +0200

    quick changes

--- javascript.js
+++ javascript.js
@@ -1,21 +1,3 @@
-"use strict";
-
-  const btn = document.getElementsByClassName('nigga');
-
-  let index = 0;
-
-  const colors = ['red', 'gray'];
-
-  for(var e = 0; e < btn.length; e++) {
-    btn.addEventListener('click', onClick)
-  }
-
-  function onClick() {
-    btn.style.backgroundColor = colors[index];
-    btn.style.color = 'white';
-
-    index = index >= colors.length - 1 ? 0 : index + 1;
-  };
 
 // ============================ Collapsible Menu
 

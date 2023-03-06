64abccb6e63b37645d0ab203ec766eda7a5ca7a5
Vasko Blazhev <vbblajev@gmail.com>
2023-03-06 22:47:59 +0200

    About Page Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{html,js}'],
+  content: ['./**/*.{html,js,css}'],
   theme: {
     screens: {
       sm: "480px",
